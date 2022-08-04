c8e6c12892b207554e65ed44a9ba8664d982aa1e
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2022-08-04 14:26:43 +0900

    init: first commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
+
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+}
